5753f37ca70583e9daaa3da5599c2394714c0df9
musahussain <mdmuslimhussain1@gmail.com>
2022-08-03 23:14:14 +0600

    initial commit

---
+++ src/Pages/Appointment/Appointment.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Appointment = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold">Appointment</h1>
+        </div>
+    );
+};
+
+export default Appointment;
---
+++ src/SharedComponent/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-3xl font-bold'>Header</h1>
+        </div>
+    );
+};
+
+export default Header;
