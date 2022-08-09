6c280045ec931615b2e70cf7b0e6e572606a98dc
musahussain <mdmuslimhussain1@gmail.com>
2022-08-09 21:30:35 +0600

    created appointment page

--- src/Pages/Appointment/Appointment.js
+++ src/Pages/Appointment/Appointment.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+import AppointmentBanner from './AppointmentBanner';
+import AvailableAppointments from './AvailableAppointments';
 
 const Appointment = () => {
+    const [date, setDate] = useState(new Date());
     return (
         <div>
-            <h1 className="text-3xl font-bold">Appointment</h1>
+            <AppointmentBanner date={date} setDate={setDate}></AppointmentBanner>
+            <AvailableAppointments date={date}></AvailableAppointments>
         </div>
     );
 };
